cac8d6426d8dc6f34753ba8f1568d90c9a1f3e2d
Ajay Singh <daltavida@gmail.com>
2021-10-11 17:43:00 +1030

    Optimise smooth scrolling code by using modern way

--- script.js
+++ script.js
@@ -34,12 +34,5 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  e.target.getBoundingClientRect();
-
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: "smooth",
-  });
+  section1.scrollIntoView({ behavior: "smooth" });
 });
