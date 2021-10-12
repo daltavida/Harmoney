36a9316e4333549c7a65b0fabb568f66d519d217
Ajay Singh <daltavida@gmail.com>
2021-10-12 18:44:00 +1030

    Use one event listener instead of multiple

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-document.querySelectorAll(".nav__link").forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = this.getAttribute("href");
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
