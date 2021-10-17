56028f287c42cd7d4376ca421b3d650926b5d4fa
Ajay Singh <daltavida@gmail.com>
2021-10-17 23:49:00 +1030

    Trigger sticky nav earlier with smaller root margin

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: "-90px",
 });
 
 headerObserver.observe(header);
