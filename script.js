c4366e7bf6001cf995c2022903194f1b6d84ff8b
Ajay Singh <daltavida@gmail.com>
2021-10-24 16:54:00 +1030

    Add function to activate slide dots

--- script.js
+++ script.js
@@ -144,11 +144,42 @@ const btnRight = document.querySelector(".slider__btn--right");
 let currentSlide = 0;
 const maxSlide = slides.length;
 
+const dotContainer = document.querySelector(".dots");
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`)
   );
+  activateDot(slide);
 };
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+createDots();
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+  }
+});
 
 goToSlide(0);
 
@@ -183,16 +214,3 @@ document.addEventListener("keydown", function (e) {
     nextSlide();
   }
 });
-
-const dotContainer = document.querySelector(".dots");
-
-const createDots = function () {
-  slides.forEach(function (_, i) {
-    dotContainer.insertAdjacentHTML(
-      "beforeend",
-      `<button class="dots__dot" data-slide="${i}"></button>`
-    );
-  });
-};
-
-createDots();
